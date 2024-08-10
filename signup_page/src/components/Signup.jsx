4c6ddfddf94f8c36485e8eae3328ec4c870131f5
Vishnuvarthan M <131575256+Vishnu-Varthan-05@users.noreply.github.com>
2024-08-10 15:27:30 +0530

    added some in signup page

--- signup_page/src/components/Signup.jsx
+++ signup_page/src/components/Signup.jsx
@@ -1,55 +1,75 @@
 import { useState } from 'react';
 import axios from 'axios';
-import './Signup.css'
+import './Signup.css';
+
 const Signup = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-        try{
-            const res = await axios.post('http:/localhost:3000/signup', {
+        try {
+            const res = await axios.post('http://localhost:3000/signup', {
                 email,
                 password,
             });
-    
-            if(res.status === 200){
+
+            if (res.status === 200) {
                 alert('Signed up successfully');
+                // Reset the form
+                setEmail('');
+                setPassword('');
             } else {
                 alert(`Error: ${res.data.message}`);
             }
-        } catch(err){
-            alert(`Error: ${err.res ? err.res.data.message : err.message}`)
+        } catch (err) {
+            alert(`Error: ${err.response ? err.response.data.message : err.message}`);
         }
-
     };
 
     return (
-        <>
-            <div className="content">
-                <div id="signup">
-                    <h1>Sign Up</h1>
-                </div>
-                
-                <div id="form">
-                    <input className="email" type="text" placeholder="Email" value = {email} onChange={(e) => setEmail(e.target.value)}/>
-                    <input className="password" type="password" placeholder="Password"  value={password} onChange={(e) => setPassword(e.target.value)}/>
-                </div>
+        <div className="content">
+            <div id="signup">
+                <h1>Sign Up</h1>
+            </div>
+            <form id="form" onSubmit={handleSubmit}>
+                <input
+                    className="email"
+                    type="text"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                    className="password"
+                    type={showPassword ? "text" : "password"}
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
                 <div id="extra_details">
                     <div id="checkbox">
-                        <input type="checkbox" name="show_password" id="n1" />
+                        <input
+                            type="checkbox"
+                            name="show_password"
+                            id="n1"
+                            checked={showPassword}
+                            onChange={() => setShowPassword(!showPassword)}
+                        />
                         <label htmlFor="n1">Show password</label>
                     </div>
-                    <div id="pass"><p>Forgot password?</p></div>
+                    <div id="pass">
+                        <p>Forgot password?</p>
+                    </div>
                 </div>
                 <div id="button">
-                    <button onClick={handleSubmit}>Sign In</button>
+                    <button type="submit">Sign Up</button>
                 </div>
-            </div>
-        </>
+            </form>
+        </div>
     );
-}
-
+};
 
 export default Signup;
